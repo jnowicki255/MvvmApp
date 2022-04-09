777470c0aaf478411a6e742a7ae958941ed9505c
Jacek Nowicki <jacek.nowicki@acciosoftware.pl>
2022-04-09 08:17:50 +0200

    Geolocalization

--- MvvmApp/MvvmApp/AssemblyInfo.cs
+++ MvvmApp/MvvmApp/AssemblyInfo.cs
@@ -1,3 +1,16 @@
+using Android.App;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadPhoneState)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessWifiState)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessLocationExtraCommands)]
+[assembly: UsesPermission(Android.Manifest.Permission.ControlLocationUpdates)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessMockLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)] 
--- MvvmApp/MvvmApp/ViewModels/SettingsViewModel.cs
+++ MvvmApp/MvvmApp/ViewModels/SettingsViewModel.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace MvvmApp.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
+        private string location;
+        public string Location
+        {
+            get => location;
+            set => SetProperty(ref location, value);
+        }
         public SettingsViewModel()
         {
             Title = "Settings";
+            GetGeolocation().Wait();
+        }
+        public async Task GetGeolocation()
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    Location = $"Latitute: {location.Latitude}, " +
+                        $"Longtitude: {location.Longitude}, " +
+                        $"Altitude: {location.Altitude}";
+                }
+                else
+                {
+                    Location = "Location is null";
+                }
+            }
+            catch (Exception ex)
+            {
+                Location = $"Cannot read location {ex.ToString()}";
+            }
         }
     }
 }
